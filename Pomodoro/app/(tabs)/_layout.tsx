706bf9c46614c03dc163fa09293e355ac4866e06
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-06-23 19:22:36 +0300

    Update layout

--- Pomodoro/app/(tabs)/_layout.tsx
+++ Pomodoro/app/(tabs)/_layout.tsx
@@ -1,15 +1,22 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import { useFonts } from 'expo-font';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    Pacifico: require('../../assets/fonts/Pacifico-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null; // тук може да върнеш и custom spinner/loading
+  }
 
   return (
     <Tabs
@@ -20,7 +27,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
