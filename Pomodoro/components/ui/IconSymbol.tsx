3ac0f52b5691d3020011d6e3eadf8e9258e7e437
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-06-26 16:48:29 +0300

    Symbol for refresh button

--- Pomodoro/components/ui/IconSymbol.tsx
+++ Pomodoro/components/ui/IconSymbol.tsx
@@ -19,6 +19,7 @@ const MAPPING = {
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
   'gearshape.fill': 'settings',
+  'arrow.clockwise': 'refresh',
 } as IconMapping;
 
 /**
